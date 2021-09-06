2d44f98ce113f79e0deae3ac3980a3e83c7e8183
Fadi_Nayef <adraf38@hotmail.com>
2021-09-06 20:17:13 +0300

    "Location Methods have been Moved to The Main Activity "

--- app/src/main/java/com/example/floklores/MainActivity.java
+++ app/src/main/java/com/example/floklores/MainActivity.java
@@ -1,14 +1,114 @@
 package com.example.floklores;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+    private Location currentLocation;
+    FusedLocationProviderClient fusedLocationClient;
+    int PERMISSION_ID = 99;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        askForPermissionToUseLocation();
+        configureLocationServices();
+        askForLocation();
+
+
+        Button addItem =  findViewById(R.id.toAddItemButton);
+        addItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toAddItem = new Intent (getApplicationContext(),AddItemActivity.class);
+                startActivity(toAddItem);
+            }
+        });
+    }
+    private void configureLocationServices() {
+        fusedLocationClient  = LocationServices.getFusedLocationProviderClient(this);
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void askForPermissionToUseLocation(){
+        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 2);
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+    }
+    @SuppressLint("MissingPermission")
+    public void askForLocation() {
+
+        LocationRequest locationRequest;
+        LocationCallback locationCallback;
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(60000);
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                currentLocation = locationResult.getLastLocation();
+                Log.i("Location", currentLocation.toString());
+
+                Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+
+                try {
+                    List<Address> addresses = geocoder.getFromLocation(currentLocation.getLatitude(), currentLocation.getLongitude(), 10);
+
+                    Log.e("Location","Your Address is " + " " + addresses);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, getMainLooper());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        askForPermissionToUseLocation();
+//        configureLocationServices();
+//        askForLocation();
     }
 }
